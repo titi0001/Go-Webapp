009ef0ca2f6231b603391bd35b6ac3c6df5d707c
Thiago <titiura@gmail.com>
2024-04-01 16:28:25 -0300

    feat: add main

--- main.go
+++ main.go
@@ -1,34 +1,11 @@
 package main
 
 import (
-	"database/sql"
 	"html/template"
 	"net/http"
-	"log"
 
-	_ "github.com/lib/pq"
+	"github.com/titi001/models"
 )
-
-type Produto struct {
-	Id         int
-	Nome       string
-	Descricao  string
-	Preco      float64
-	Quantidade int
-}
-
-
-func conectaDb() (*sql.DB, error) {
-	// conexao := "postgres://postgres:123456789@localhost/alura_loja?sslmode=disable"
-	conexao := "user=postgres dbname=alura_loja password=123456789 host=localhost port=42381 sslmode=disable"
-	db, err := sql.Open("postgres", conexao)
-	if err != nil {
-		log.Printf("Erro ao abrir a conexão com o banco de dados: %v\n", err)
-		return nil, err
-	}
-	log.Println("Conexão bem-sucedida com o banco de dados!")
-	return db, nil
-}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
@@ -38,51 +15,7 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	db, err := conectaDb()
-	if err != nil {
-		// Tratar erro de conexão
-		http.Error(w, "Erro de conexão com o banco de dados", http.StatusInternalServerError)
-		return
-	}
-	defer db.Close()
 
-	selectDeTodosProdutos, err := db.Query("SELECT * FROM produtos")
-	if err != nil {
-		// Tratar erro de consulta
-		http.Error(w, "Erro ao consultar o banco de dados", http.StatusInternalServerError)
-		return
-	}
-	defer selectDeTodosProdutos.Close()
-
-	var produtos []Produto
-
-	for selectDeTodosProdutos.Next() {
-		var id, quantidade int
-		var nome, descricao string
-		var preco float64
-
-		err := selectDeTodosProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
-		if err != nil {
-			// Tratar erro de leitura
-			http.Error(w, "Erro ao ler os dados do banco de dados", http.StatusInternalServerError)
-			return
-		}
-
-		p := Produto{
-			Nome:       nome,
-			Descricao:  descricao,
-			Preco:      preco,
-			Quantidade: quantidade,
-		}
-
-		produtos = append(produtos, p)
-	}
-
-	err = temp.ExecuteTemplate(w, "index", produtos)
-	if err != nil {
-		// Tratar erro de execução do template
-		http.Error(w, "Erro ao executar o template", http.StatusInternalServerError)
-		return
-	}
+	todosOsProdutos := models.BuscaTodosOsProdutos
+	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
 }
-
