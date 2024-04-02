f33bfdc356a88b20a33b7fba00bb1df31b6b007f
Thiago <titiura@gmail.com>
2024-04-02 12:15:33 -0300

    refactor: add log error

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -10,7 +10,12 @@ import (
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
-
-	todosOsProdutos := models.BuscaTodosOsProdutos()
+	todosOsProdutos, err := models.BuscaTodosOsProdutos()
+	if err != nil {
+		// Tratar erro de busca de produtos
+		http.Error(w, "Erro ao buscar os produtos", http.StatusInternalServerError)
+		return
+	}
+	
 	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
 }
--- models/produtos.go
+++ models/produtos.go
@@ -12,34 +12,39 @@ type Produto struct {
 	Quantidade int
 }
 
-func BuscaTodosOsProdutos() []Produto {
-	db := db.ConectaDb()
+func BuscaTodosOsProdutos() ([]Produto, error) {
+	db, err := db.ConectaDb()
+	if err != nil {
+		return nil, err // Retornar erro de consulta
+	}
 
 	selectDeTodosOsProdutos, err := db.Query("SELECT * FROM produtos")
 	if err != nil {
-		panic(err.Error())
+		return nil, err // Retornar erro de consulta
 	}
+	defer selectDeTodosOsProdutos.Close()
 
-	p := Produto{}
-	produtos := []Produto{}
+	var produtos []Produto
 
 	for selectDeTodosOsProdutos.Next() {
 		var id, quantidade int
 		var nome, descricao string
 		var preco float64
 
-		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		err := selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
 		if err != nil {
-			panic(err.Error())
+			return nil, err // Retornar erro de leitura
 		}
 
-		p.Nome = nome
-		p.Descricao = descricao
-		p.Preco = preco
-		p.Quantidade = quantidade
+		p := Produto{
+			Nome:       nome,
+			Descricao:  descricao,
+			Preco:      preco,
+			Quantidade: quantidade,
+		}
 
 		produtos = append(produtos, p)
 	}
 	defer db.Close()
-	return produtos
+	return produtos, nil
 }
