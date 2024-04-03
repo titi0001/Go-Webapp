591d59511db77c4d7c657ecd745d6afd5a6f72fd
Thiago <titiura@gmail.com>
2024-04-03 10:46:55 -0300

    feat: create routes new product

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 
 	"github.com/titi0001/Go-REST-ORM/models"
@@ -16,6 +18,32 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Erro ao buscar os produtos", http.StatusInternalServerError)
 		return
 	}
-	
+
 	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertido, err := strconv.ParseFloat(preco, float64)
+		if err != nil {
+			log.Println("Erro na conversão do preço:", err)
+		}
+		quantidadeConvertido, err := strconv.Atoi(quantidade, int)
+		if err != nil {
+			log.Println("Erro na conversão de quantidade:", err)
+		}
+
+		models.CriarNovoProduto(nome, descricao, precoConvertido, quantidadeConvertido)
+	}
+	http.Redirect(w, r, "/", 301)
+
 }
